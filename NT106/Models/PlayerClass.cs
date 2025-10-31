140508a7c112a2387e0dab5f053427962dba294a
Bùi Công Định <buicongdinh330@gmail.com>
2025-10-31 13:34:24 +0700

    Them lop room va player

---
+++ NT106/Models/PlayerClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NT106.Models
+{
+    public class PlayerClass
+    {
+        public string Uid { get; set; }
+        public string InGameName { get; set; }
+        public string Money { get; set; }
+        public Image Avatar { get; set; }
+        public bool IsHost { get; set; }
+
+    }
+}
---
+++ NT106/Models/RoomClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NT106.Models
+{
+    public class RoomClass
+    {
+        public string RoomId { get; set; }
+        public string HostUid { get; set; }
+        public List<PlayerClass> playerClasses { get; set; }
+        public string Status { get; set; }
+    }
+}
